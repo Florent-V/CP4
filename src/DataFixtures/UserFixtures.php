a51183e8036f19b4e3a7afeb215a96bc30c55154
Florent-Vasseur <vasseurflorent@gmail.com>
2023-02-01 09:36:55 +0100

    inscription, connexion, oublie de mot de passe ok

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    public static int $userIndex = 0;
+
+    public function __construct(private readonly  UserPasswordHasherInterface $passwordHasher)
+    {
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create('fr_FR');
+
+        $admin = new User();
+        $admin->setPseudo('FloV5');
+        $admin->setFirstName('Florent');
+        $admin->setLastName('Vasseur');
+        $admin->setEmail('florent@mail.fr');
+        $admin->setPhone($faker->phoneNumber());
+        $admin->setIsVerified(true);
+        $admin->setRoles((array)'ROLE_ADMIN');
+
+        $hashedPassword = $this->passwordHasher->hashPassword(
+            $admin,
+            'password'
+        );
+        $admin->setPassword($hashedPassword);
+
+        $manager->persist($admin);
+
+        for ($i = 0; $i <= 20; $i++) {
+            self::$userIndex++;
+            $user = new User();
+
+            $firstName = $faker->firstName();
+            $user->setPseudo($firstName . $faker->year());
+            $user->setFirstName($firstName);
+            $user->setLastName($faker->lastName());
+            $user->setEmail('user' . self::$userIndex . '@mail.fr');
+            $user->setPhone($faker->phoneNumber());
+            $user->setIsVerified(true);
+            $user->setRoles((array)'ROLE_USER');
+
+            $hashedPassword = $this->passwordHasher->hashPassword(
+                $user,
+                'motdepasse'
+            );
+            $user->setPassword($hashedPassword);
+
+            $manager->persist($user);
+            $this->addReference('user_' . self::$userIndex, $user);
+        }
+        $manager->flush();
+    }
+}
