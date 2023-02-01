450d6523d16f05e2ed581cc39165e8da78f3361a
Florent-Vasseur <vasseurflorent@gmail.com>
2023-02-01 14:12:20 +0100

    redirection si non identifier

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,12 +2,14 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    #[IsGranted('ROLE_USER')]
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
