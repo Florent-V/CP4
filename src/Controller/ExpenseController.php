9640d6497a17261672fa4b30a3b012f97effeaa1
Florent-Vasseur <vasseurflorent@gmail.com>
2023-02-03 11:20:12 +0100

    ajout dépense ok

---
+++ src/Controller/ExpenseController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Expense;
+use App\Entity\Splitter;
+use App\Form\ExpenseType;
+use App\Repository\ExpenseRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use DateTime;
+
+#[Route('/expense')]
+class ExpenseController extends AbstractController
+{
+    #[Route('/', name: 'app_expense_index', methods: ['GET'])]
+    public function index(ExpenseRepository $expenseRepository): Response
+    {
+        return $this->render('expense/index.html.twig', [
+            'expenses' => $expenseRepository->findAll(),
+        ]);
+    }
+
+    #[Route(
+        '/splitter/{id}/new',
+        name: 'app_expense_new',
+        requirements: [
+            'id' => '^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$'
+        ],
+        methods: ['GET', 'POST']
+    )]
+    public function new(
+        Request $request,
+        Splitter $splitter,
+        ExpenseRepository $expenseRepository
+    ): Response {
+
+        $expense = new Expense();
+        $form = $this->createForm(ExpenseType::class, $expense, [
+            'splitter' => $splitter
+        ]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $expense->setSplitter($splitter);
+            $expense->setCreatedAt(new DateTime('now'));
+            $expense->setDevise('€');
+            $expenseRepository->save($expense, true);
+
+            return $this->redirectToRoute('app_splitter_show', ['id' => $splitter->getId()], Response::HTTP_SEE_OTHER);
+        }
+        return $this->render('expense/new.html.twig', [
+            'expense' => $expense,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_expense_show', methods: ['GET'])]
+    public function show(Expense $expense): Response
+    {
+        return $this->render('expense/show.html.twig', [
+            'expense' => $expense,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_expense_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Expense $expense, ExpenseRepository $expenseRepository): Response
+    {
+        $form = $this->createForm(ExpenseType::class, $expense);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $expenseRepository->save($expense, true);
+
+            return $this->redirectToRoute('app_expense_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('expense/edit.html.twig', [
+            'expense' => $expense,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_expense_delete', methods: ['POST'])]
+    public function delete(Request $request, Expense $expense, ExpenseRepository $expenseRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $expense->getId(), $request->request->get('_token'))) {
+            $expenseRepository->remove($expense, true);
+        }
+
+        return $this->redirectToRoute('app_expense_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/ExpenseType.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Expense;
+use App\Entity\ExpenseCategory;
+use App\Entity\Splitter;
+use App\Entity\User;
+use App\Repository\CategoryRepository;
+use App\Repository\UserRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ExpenseType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $splitter = $options['splitter'];
+
+        $builder
+            ->add('name', null, [
+                'row_attr' => ['class' => 'form-floating mb-3 text-dark'],
+                'label' => 'Nom',
+                'attr' => ['placeholder' => 'pseudo']
+            ])
+            ->add('madeAt', DateType::class, [
+                'row_attr' => ['class' => 'form-floating mb-3 text-dark'],
+                'label' => 'Date',
+                'attr' => ['placeholder' => 'date..'],
+                'widget' => 'single_text',
+                'format' => 'yyyy-MM-dd',
+            ])
+            ->add('amount', MoneyType::class, [
+                'row_attr' => ['class' => 'mb-3 text-light'],
+                'label' => 'Montant',
+                'attr' => ['placeholder' => 'pseudo']
+            ])
+            ->add('paidBy', EntityType::class, [
+                'class' => User::class,
+                'row_attr' => ['class' => 'form-floating mb-3  text-dark'],
+                'label' => 'Payé par :',
+                'query_builder' => function (UserRepository $userRepository) use ($splitter) {
+                    return $userRepository->createQueryBuilder('u')
+                        ->innerJoin('u.splitters', 's')
+                        ->where('s.uniqueId = :uniqueId')
+                        ->setParameter('uniqueId', $splitter->getUniqueId())
+                    ->orderBy('u.firstName', 'ASC');
+                },
+                'choice_label' => function (User $user) {
+                    return $user->getFirstName() . ' ' . $user->getLastName();
+                }
+            ])
+            ->add('category', EntityType::class, [
+                'class' => ExpenseCategory::class,
+                'required' => true,
+                'row_attr' => ['class' => 'form-floating mb-3  text-dark'],
+                'label' => 'Catégorie',
+                'query_builder' => function (CategoryRepository $repository) {
+                    return $repository->createQueryBuilder('c')
+                    ->orderBy('c.name', 'ASC');
+                },
+                'choice_label' => 'name'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Expense::class,
+            'splitter' => null,
+        ]);
+    }
+}
