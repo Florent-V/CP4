867b6bde6990b1fb04d10fd60d24ff30d7b84b2f
Florent-V <vasseurflorent@gmail.com>
2024-03-17 16:28:35 +0100

    install import map

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,11 +6,11 @@ use App\Entity\User;
 use App\Form\SearchBarType;
 use App\Repository\SplitterRepository;
 use Knp\Component\Pager\PaginatorInterface;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class HomeController extends AbstractController
 {
