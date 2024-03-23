0b0efbe9c55dbfbd20a0d56575a0c1aa99b22334
Florent-V <vasseurflorent@gmail.com>
2024-03-23 08:06:06 +0100

    installation asset mapper

--- importmap.php
+++ importmap.php
@@ -26,4 +26,17 @@ return [
     '@symfony/ux-live-component' => [
         'path' => './vendor/symfony/ux-live-component/assets/dist/live_controller.js',
     ],
+    'bootstrap/dist/css/bootstrap.min.css' => [
+        'version' => '5.3.3',
+        'type' => 'css',
+    ],
+    'bootstrap/dist/js/bootstrap.min.js' => [
+        'version' => '5.3.3',
+    ],
+    '@popperjs/core' => [
+        'version' => '2.11.8',
+    ],
+    'bootstrap' => [
+        'version' => '5.3.3',
+    ],
 ];
