bb8dffe9e8f38c30e2f7e3031af7f828f15cfa7f
Florent-V <vasseurflorent@gmail.com>
2024-04-01 10:14:57 +0200

    feat crud splitter admin ok

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -30,16 +31,19 @@ class LoginController extends AbstractController
     #[Route('/redirectAfterLogin', name: 'app_login_redirect', methods: ['GET'])]
     public function redirectAfterLogin(): Response
     {
-//        if ($this->isGranted('ROLE_ADMIN')) {
-//            return $this->redirectToRoute('admin_app_dashboard');
-//        }
+        if ($this->isGranted('ROLE_ADMIN')) {
+            return $this->redirectToRoute('admin_app_splitter_index');
+        }
         return $this->redirectToRoute('app_home');
     }
 
+    /**
+     * @throws Exception
+     */
     #[Route('/logout', name: 'app_logout', methods: ['GET'])]
     public function logout(): void
     {
         // controller can be blank: it will never be called!
-        throw new \Exception('Don\'t forget to activate logout in security.yaml');
+        throw new Exception('Don\'t forget to activate logout in security.yaml');
     }
 }
