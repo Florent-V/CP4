40be3235a4d51358e0ba2521cc8f7a0cd3ae39a0
Florent-V <vasseurflorent@gmail.com>
2025-05-25 08:24:40 +0200

    update importe Attribute/Route in symfony

--- src/Controller/Admin/SplitterController.php
+++ src/Controller/Admin/SplitterController.php
@@ -17,7 +17,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Address;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/splitter')]
 class SplitterController extends AbstractController
