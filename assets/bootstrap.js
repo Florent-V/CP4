1e26c4e93fc36c3ab72ac83d9ab193f831cb02ef
Florent-V <vasseurflorent@gmail.com>
2025-05-30 16:32:33 +0200

    fix issue with stimulus after update recipes

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,7 +1,4 @@
 import { startStimulusApp } from '@symfony/stimulus-bundle';
-
-const app = startStimulusApp();
-import { startStimulusApp } from '@symfony/stimulus-bundle';
 import Clipboard from '@stimulus-components/clipboard'
 
 const app = startStimulusApp();
