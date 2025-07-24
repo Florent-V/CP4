a8db59c0d84b4b35bcde84a3bd29c475cb018b18
Florent-V <vasseurflorent@gmail.com>
2025-07-24 15:39:58 +0200

    feat(share): add single page for share, like modal but full page

--- src/Controller/Splitter/ShareController.php
+++ src/Controller/Splitter/ShareController.php
@@ -5,14 +5,10 @@ namespace App\Controller\Splitter;
 use App\Entity\Splitter;
 use App\Entity\User;
 use App\Enum\Role;
-use App\Form\ShareSplitterFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Address;
-use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Service\SplitterAccessManager;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -28,9 +24,6 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 )]
 class ShareController extends AbstractController
 {
-    /**
-     * @throws TransportExceptionInterface
-     */
     public function __invoke(
         Request $request,
         Splitter $splitter,
@@ -43,35 +36,8 @@ class ShareController extends AbstractController
         $user = $this->getUser();
         $accessManager->checkReadAccess($splitter, $user);
 
-        $form = $this->createForm(ShareSplitterFormType::class);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $data = $form->getData();
-            $email = (new TemplatedEmail())
-                ->from(new Address('florent@f5t.fr', 'Kopeck Bot'))
-                ->to($data['email'])
-                ->subject('On vous invite dans un Splitter !')
-                ->htmlTemplate('splitter/shareEmail.html.twig')
-                ->context([
-                    'splitter' => $splitter,
-                    'user' => $user,
-                ]);
-            $mailer->send($email);
-
-            $this->addFlash('success', '🙂 Votre ami a bien été prévenu par mail !');
-
-            return $this->redirectToRoute(
-                'app_splitter_show',
-                [
-                    'id' => $splitter->getId()
-                ],
-                Response::HTTP_SEE_OTHER
-            );
-        }
         return $this->render('splitter/share.html.twig', [
             'splitter' => $splitter,
-            'form' => $form,
         ]);
     }
 }
